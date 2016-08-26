bb84add2acdd3162959c485c9a9fc7ec23c42ec2
Knayz <pesterev.andruha@gmail.com>
2016-08-26 14:08:54 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 26.08.16
+ * Time: 13:49
+ */
+
+ini_set("display_errors", 1);
+ini_set("erro_reporting", -1);
+
+var_dump($_REQUEST);
